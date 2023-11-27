445b3ece15204d8accebe66dc0c8da81f1e55ae0
Basliel Tegegn <basgolilo5@gmail.com>
2023-11-27 04:12:41 -0800

    fix

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *low = list;
+	listint_t *high = list;
+
+	if (!list)
+		return (0);
+
+	while (low && high && high->next)
+	{
+		low = low->next;
+		high = high->next->next;
+		if (low == high)
+			return (1);
+	}
+
+	return (0);
+}

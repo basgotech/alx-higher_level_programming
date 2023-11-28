07ac3535881f43455372d4015b6f46434c1a42bc
Basliel Tegegn <basgolilo5@gmail.com>
2023-11-28 10:50:48 -0800

    ad print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 # Print all possible different combinations of two digits
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=', ')
-
-# Print a new line after the last combination
-print()
+        if tens_digit == 8:
+            print("{:d}{:d}".format(tens_digit, ones_digit))
+        else:
+            print("{:d}{:d}".format(tens_digit, ones_digit), end=', ')

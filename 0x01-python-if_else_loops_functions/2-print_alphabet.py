9aae4395b2a6ebbb6c830c07d4fab25aec9a7f55
Basliel Tegegn <basgolilo5@gmail.com>
2023-11-28 09:45:56 -0800

    adding print alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-alphabet = ''.join(chr(char) for char in range(ord('a'), ord('z') + 1))
-print(alphabet, end='')
+for char in range(ord('a'), ord('z') + 1):
+    print(chr(char), end='')

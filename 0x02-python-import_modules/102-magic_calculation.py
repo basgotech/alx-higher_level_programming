e20281eb1a0c814887a573a92087ae951a263b3a
Basliel Tegegn <basgolilo5@gmail.com>
2023-11-30 11:02:32 -0800

    adding magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    
     from magic_calculation_102 import add, sub
-
     if a < b:
-        magic = add(a, b)
-        for b in range(4, 6):
-            magic = add(magic, b)
-        return (magic)
-
+        mgic = add(a, b)
+        for i in range(4, 6):
+            mgic = add(mgic, i)
+        return (mgic)
     else:
-        return(sub(a, b))
+        return (sub(a, b))

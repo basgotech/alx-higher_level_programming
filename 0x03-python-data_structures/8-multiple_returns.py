9a680d93f65c865959a754716e9821e1342c475f
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-03 13:36:23 -0800

    mul ret

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return 0, None
+    my_sent = ()
+    if len(sentence) == 0:
+        my_sent = 0, "None"
     else:
-        return len(sentence), sentence[0]
+        my_sent = len(sentence), sentence[0]
+    return my_sent

de2581dd3565ff3b5460bd9e6fbe8b50df8b4af4
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-06 09:17:17 -0800

    adding sq mat

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix_deploy_to = matrix.copy()
+    leng = len(matrix)
+    for i in range(leng):
+        matrix_deploy_to[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (matrix_deploy_to)

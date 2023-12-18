4b23baf33526b5fc3e6508e56d3f5b7a90278d5e
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-18 02:34:26 -0800

    adding safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count_len = 0
+        for x in range(x):
+            print("{}".format(my_list[x]), end="")
+            count_len += 1
+        print()
+        return count_len
+    except IndexError:
+        print()
+        return count_len

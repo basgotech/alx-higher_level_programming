dbda0c65ef8d4dc8317e2e325a61ddc23942e1e7
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-18 02:48:51 -0800

    adding p

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count_len = 0
+    count = 0
     try:
         for i in range(x):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
-                count_len += 1
+                count += 1
         print()
-        return count_len
+        return count
     except IndexError:
-        print()
-        return count_len
+        return count

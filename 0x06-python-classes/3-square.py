f3d72a22fffe9cb774d0db433bced879162e91fb
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-19 04:39:39 -0800

    adding 3 sq

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Constract a class Square."""
+
 
 class Square:
-    """Show a class Square."""
-
+    """body a square class."""
 
     def __init__(self, size=0):
-        """
-        Initialize a square.
+        """Initialize a new square.
 
         Args:
-        - size (int): Optional size of the square (default is 0).
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """Calculate the area of the square."""
+        """Return the current area of the square."""
         return (self.__size * self.__size)

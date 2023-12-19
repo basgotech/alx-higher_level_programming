d366c5da9d00bf87c8935bb1ddce5a08dc398e5f
Basliel Tegegn <basgolilo5@gmail.com>
2023-12-19 03:58:20 -0800

    adding 1 sq

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,15 +2,14 @@
 
 """Define a class Square."""
 
-class Square:
-     
-     """Represent a square."""
 
-def __init__(self, size):
-         
-         """Initialize a new Square.
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square class.
 
         Args:
             size (int): The size of the new square.
         """
-         self.__size = size
+        self.__size = size

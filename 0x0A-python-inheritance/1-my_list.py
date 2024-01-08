6246e3df22f0960b7ece4bf2e274762ca7eadf04
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-08 08:02:16 -0800

    adding list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Print the list in ascending sorted order."""
 
+
 class MyList(list):
-     """a subclass of list"""
+    """a subclass of list"""
     def __init__(self):
         """initializes the object"""
         super().__init__()
 
     def print_sorted(self):
-        """Print the list in ascending sorted order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """prints the sorted list"""
+        print(sorted(self))

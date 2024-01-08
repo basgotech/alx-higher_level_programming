1b0cc7fa8dad8443bea0c720546069e00436b9e4
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-08 08:53:13 -0800

    adding geo

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
 
-class BaseGeometry:
-    def area(self):
-        """
-        Raises an Exception with the message 'area() is not implemented'.
 
-        This method should be overridden by subclasses to provide
-        a specific implementation for calculating the area.
-        """
-        raise Exception('area() is not implemented')
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")

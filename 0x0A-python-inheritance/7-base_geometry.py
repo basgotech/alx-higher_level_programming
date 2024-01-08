0862ff9604d4fac531056df752461289a2de8f24
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-08 09:05:27 -0800

    adding

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""class that Defines a base geometry"""
 
 
 class BaseGeometry:
-    """Reprsent base geometry."""
+    """Reprsent base geometry definition."""
 
     def area(self):
         """Not yet implemented."""
@@ -11,7 +11,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
-
         Args:
             name (str): The name of the parameter.
             value (int): The parameter to validate.

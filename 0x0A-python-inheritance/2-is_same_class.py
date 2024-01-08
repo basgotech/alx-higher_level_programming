c2f0a23be2f1c39b0124ea5bf434348975a4c876
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-08 08:16:10 -0800

    adding same class"

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,4 +3,5 @@
 
 def is_same_class(obj, a_class):
     """Check if the object is exactly an instance of the specified class."""
+    # Use the __class__ attribute to get the class of the object
     return obj.__class__ is a_class

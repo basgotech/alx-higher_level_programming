faeb31797f04ac12ab8e963db491b53f0292fa47
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-09 05:20:31 -0800

    adding json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """
 save_to_json_file module
 """
@@ -10,6 +11,5 @@ def save_to_json_file(my_obj, filename):
         my_obj: The object to be written to the file.
         filename (str): The name of the file.
     """
-    import json
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)

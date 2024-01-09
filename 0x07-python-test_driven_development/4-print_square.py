37b2f08dcd9f01ab71c71de421cfcaab8b3f5cd9
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-09 04:20:26 -0800

    adding p sq

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module to demonstrate the say_my_name function. """
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Parameters:
+    - size: An integer representing the size length of the square.
+
+    Raises:
+    - TypeError: If size is not an integer.
+    - ValueError: If size is less than 0.
+    """
+    # Check if size is an integer
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    # Check if size is less than 0
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    # Print the square
+    for _ in range(size):
+        print("#" * size)

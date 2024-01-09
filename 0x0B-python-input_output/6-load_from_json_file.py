b451f2bd2411e199c52d1336269691194c2a5e32
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-09 05:34:38 -0800

    adding jso

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save"""
-import sys
+"""
+6-load_from_json_file module
+"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+def load_from_json_file(filename):
+    """Creates an object from a JSON file.
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        object: The Python data structure represented by the JSON file.
+    """
+    import json
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)

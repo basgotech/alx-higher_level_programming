eac30227ac859ec50ff44f8f4e31839fbcb7626d
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-09 00:34:44 -0800

    adding add int

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" used to add tow numbers """
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    This function takes two arguments, a and b, and adds them together. If either a or b is a float,
+    they are first casted to integers before the addition. The default value for b is 98.
+
+    Parameters:
+    - a: An integer or float.
+    - b: An integer or float (default is 98).
+
+    Returns:
+    An integer: the addition of a and b.
+
+    Raises:
+    TypeError: If either a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b

2df9d1e58b05dbfb06fcdec3f5301e347e0361f7
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-13 11:21:04 -0800

    adding f table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,15 @@
+/*
+  Creates a table named 'first_table' if it does not already exist.
+
+  Table Structure:
+  - 'id': Integer field representing the identifier.
+  - 'name': Variable character field with a maximum length of 256 characters.
+
+  Note: This script ensures that the table is only created if it doesn't exist,
+  preventing any conflicts or errors.
+*/
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256)
+);
+

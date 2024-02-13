5e2b98a126578165f5f0b11d171c65d812e727fb
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-13 11:25:50 -0800

    adding full table"

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Shows the CREATE TABLE statement for the 'first_table' table.
+-- This command displays the SQL statement that would create the specified table,
+-- providing insights into the table's structure and constraints.
+
+SHOW CREATE TABLE first_table;

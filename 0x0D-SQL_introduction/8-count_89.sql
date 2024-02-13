818af28d6e0a20dccc1f4f56905ee2212c9e4f8a
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-13 11:35:32 -0800

    adding count

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Counts the number of records in the 'first_table' table where the 'id' is 89.
+-- This command returns the total count of rows in the 'first_table' table
+-- where the value in the 'id' column is equal to 89.
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;

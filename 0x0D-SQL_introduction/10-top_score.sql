89cdab5bc4ae889b46ce7464c712469c31417cc5
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-13 11:46:38 -0800

    adding top score"

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Retrieves the 'score' and 'name' columns from the 'second_table' table
+-- and orders the result set in descending order based on the 'score' column.
+-- This SELECT statement fetches the 'score' and 'name' columns from the
+-- 'second_table' table. The ORDER BY clause is used to sort the results
+-- in descending order based on the values in the 'score' column.
+
+SELECT score, name FROM second_table ORDER BY score DESC;

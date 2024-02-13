8b9755e326c27b206fdf80184e50b9a31bbd9a91
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-13 12:00:38 -0800

    adding abg

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,7 @@
+-- Computes the average score of all records in the 'second_table' table
+-- from the 'hbtn_0c_0' database.
+-- This SELECT statement calculates the average score using the AVG() function.
+-- The result is assigned the column name 'average'.
+-- Note: Replace 'hbtn_0c_0' with the actual name of your database.
+
+SELECT AVG(score) AS average FROM hbtn_0c_0.second_table;

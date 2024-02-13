ce12453a710f6e5ec16b164e31115fd972187a77
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-13 11:28:16 -0800

    adding list val

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Retrieves all records from the 'first_table' table.
+-- This SELECT statement returns all columns and rows from the specified table,
+-- allowing for a comprehensive view of the data stored within 'first_table'.
+
+SELECT * FROM first_table;

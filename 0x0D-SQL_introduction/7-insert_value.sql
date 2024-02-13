45df49434d7785f89adc6e99ca2d0272b7c1669b
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-13 11:32:37 -0800

    adding insert val

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Inserts a new record into the 'first_table' table.
+-- Inserts the values (89, 'Best School') into the 'id' and 'name' columns
+-- of the 'first_table' table, creating a new entry in the database.
+
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');

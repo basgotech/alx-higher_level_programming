f1c793308e0c3dda402e6030beaa1132b2815b95
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-14 11:04:47 -0800

    adding

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Specify the database name as an argument when running the script
+
+-- List all shows with their linked genre IDs or NULL if no genre is linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

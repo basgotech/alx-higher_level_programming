836bc4f8f66f1a0a178b07183e3838b8deacbe9c
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-14 10:50:12 -0800

    adding

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,8 @@
 -- Specify the database name as an argument
 
 -- List all cities with corresponding state names using JOIN method
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;

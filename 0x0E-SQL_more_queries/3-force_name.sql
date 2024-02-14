7f63b27273913759062de1128b807e31bab18870
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-14 10:08:46 -0800

    adding force name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,11 +1,9 @@
--- Create database hbtn_0d_2 if it does not exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Specify the database name
+USE your_database;
 
--- Create user_0d_2 if it does not exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege to user_0d_2 on hbtn_0d_2 both
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes on both
-FLUSH PRIVILEGES;
+-- Create the force_name table if it does not exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);

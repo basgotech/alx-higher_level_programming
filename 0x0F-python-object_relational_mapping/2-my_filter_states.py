4494af2da6317059323089879105fe032f96df2f
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-20 23:59:39 -0700

    adding fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" SQL query to select states by name """
+"""  SQL query to select states by name """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    my_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    ftech = my_db.cursor()
-    ftech.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+    cu = db.cursor()
+    cu.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                 .format(sys.argv[4]))
-    query = ftech.fetchall()
+    query = cu.fetchall()
     for row in query:
         print(row)
-    ftech.close()
-    my_db.close()
+    cu.close()
+    db.close()

8acdddb067cfb6161dc16748e7d2686ed7ef4a19
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-21 11:06:04 -0700

    adding model

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,20 +10,20 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """ Create engine to connect to MySQL database """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    c_eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
     """ Create session """
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=c_eng)
     session = Session()
 
     """ Query the first State object """
-    fs = session.query(State).order_by(State.id).first()
+    fs_query = session.query(State).order_by(State.id).first()
 
     """ Check if the first state exists """
-    if fs:
-        print("{}: {}".format(fs.id, fs.name))
+    if fs_query:
+        print("{}: {}".format(fs_query.id, fs_query.name))
     else:
         print("Nothing")
 

22a953fa903256bc37437971abbe4006a934b00e
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-20 23:06:41 -0700

    adding

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,13 @@ import sys
 
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    fetch = database.cursor()
-    fetch.execute("SELECT * FROM states")
-    query = fetch.fetchall()
-    for data in query:
-        print(data)
-    fetch.close()
-    database.close()
+ 
+ database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+ fetch = database.cursor()
+ fetch.execute("SELECT * FROM states")
+ query = fetch.fetchall()
+ for data in query:
+    print(data)
+ fetch.close()
+ database.close()

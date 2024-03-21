321abe9146892a84261132a0be14ac494cfe7446
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-21 10:13:40 -0700

    adding s

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-"""  lists all data from the database hbtn_0e_0_usa """
-import MySQLdb
+"""Script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
 import sys
-
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    search_query = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (search_query, ))
+    # Connect to MySQL database
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    # Create a cursor object using cursor() method
+    cur = conn.cursor()
+
+    # Construct the query with a parameterized query to avoid SQL injection
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+
+    # Execute the query with the parameter as a tuple
+    cur.execute(query, (sys.argv[4],))
+
+    # Fetch all the rows and print them
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
+    # Close cursor and connection
     cur.close()
-    db.close()
+    conn.close()

5a4fe40f38bee3d83d03f48715afdb5c243ac2db
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-20 23:18:09 -0700

    adding filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  lists all attribute from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    fetch = database.cursor()
+    fetch.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    query = fetch.fetchall()
+    for rows in query:
+        print(rows)
+    fetch.close()
+    database.close()

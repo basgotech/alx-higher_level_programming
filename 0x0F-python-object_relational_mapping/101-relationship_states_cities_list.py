619096e16bb10463693c5902074d57e4f6b97620
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-21 12:01:13 -0700

    ad

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 """
-Script to list all State objects and corresponding City objects from the database hbtn_0e_101_usa.
+Script to add a new State and City to the database.
 """
-
 import sys
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_city import City
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Create engine to connect to MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    # Create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query all State objects and their corresponding City objects
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+    """ Create a new State object """
+    newState = State(name='California')
 
-    # Close the session
-    session.close()
+    """ Create a new City object """
+    newCity = City(name='San Francisco')
+
+    """ Add the new City to the State's cities relationship """
+    newState.cities.append(newCity)
+    
+    """ Add the new State and City to the session """
+    session.add(newState)
+    session.add(newCity)
+
+    """ Commit the changes to the database """
+    session.commit()

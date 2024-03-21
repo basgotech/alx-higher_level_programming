36c71f53dce4c408ce9ee1648524183950abacfa
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-21 10:17:48 -0700

    adding safe

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Script that takes in arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument."""
+"""Script that takes in arguments and displays all values
+from hbtn_0e_0_usa """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Connect to MySQL database
+    """ Connect to MySQL database """
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -15,20 +15,20 @@ if __name__ == "__main__":
         charset="utf8"
     )
 
-    # Create a cursor object using cursor() method
+    """ Create a cursor object using cursor() method """
     cur = conn.cursor()
 
-    # Construct the query with a parameterized query to avoid SQL injection
+    """ Construct the query with a parameterized query to avoid SQL injection """
     query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
 
-    # Execute the query with the parameter as a tuple
+    """ Execute the query with the parameter as a tuple """
     cur.execute(query, (sys.argv[4],))
 
-    # Fetch all the rows and print them
+    """ Fetch all the rows and print them """
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Close cursor and connection
+    """ Close cursor and connection """
     cur.close()
     conn.close()

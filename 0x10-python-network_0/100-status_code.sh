33e9bb45edc71d32f75398db12f1d8838b5c228d
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-28 12:42:29 -0700

    adding

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the URL-
-# passed as the first argument using curl
+# Sends a GET request to a given URL
 curl -s -o /dev/null -w "%{http_code}" "$1"

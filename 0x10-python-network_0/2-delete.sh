4d391b257096ebc1c53d7b12aee193a8be686b6d
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-28 10:14:19 -0700

    add" gi

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Check if a URL is provided as argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
 # Send a DELETE request to the URL and display the body of the response
-curl -s -X DELETE $1
+curl -s-X DELETE $1

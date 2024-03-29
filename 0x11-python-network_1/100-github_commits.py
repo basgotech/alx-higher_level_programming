1be69d38d93325f2d20536e5fb41a1e4a5e18e89
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-29 10:25:39 -0700

    adding last"

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-A Python script to retrieve the 10 most recent commits of a repository from GitHub API.
+A Python script to retrieve the 10 most recent commits
 """
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
+    repos_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    params = {'per_page': 10}  # Retrieve 10 commits
-    response = requests.get(url, params=params)
+    url_grapper = f"https://api.github.com/repos/{owner_name}/{repos_name}/commits"
+    params_grapepr = {'per_page': 10}
+    res = requests.get(url_grapper, params=params_grapepr)
 
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
+    if res.status_code == 200:
+        commits_holder = res.json()
+        for commit in commits_holder:
+            sha_hol = commit['sha']
+            owner = commit['commit']['author']['name']
+            print(f"{sha_hol}: {owner}")
     else:
-        print("Error:", response.status_code)
+        print("Error:", res.status_code)

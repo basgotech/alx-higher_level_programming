55f1f2eaffdff7a6626d5c48a2a9a8109f322408
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-29 09:55:34 -0700

    ad

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# send request to url
-import urllib.request
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+"""
 import sys
+import urllib.request
 
-# Extract the URL from command line argument
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-# Send request to the URL and retrieve the response headers
-with urllib.request.urlopen(url) as response:
-    request_id = response.headers.get('X-Request-Id')
-    print(request_id)
+    r = urllib.request.Request(url)
+    with urllib.request.urlopen(r) as res:
+        print(dict(res.headers).get("X-Request-Id"))

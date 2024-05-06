8e47f6723009f84eff6dc0dba26459f065679b52
Basliel Tegegn <basgolilo5@gmail.com>
2024-05-06 00:38:46 -0700

    ad"

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+// Wait for the document to be fully loaded
+$(document).ready(function(){
+    $('div#toggle_header').click(function(){
+        if ($('header').hasClass('red')) {
+            $('header').removeClass('red').addClass('green');
+        } else {
+            $('header').removeClass('green').addClass('red');
+        }
+    });
+});
